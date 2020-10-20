61dd4787f6d59579d395ce669b9b72d8152a1b08
unknown <jessop.lucas@gmail.com>
2020-10-20 17:02:25 -0600

    finished react 1 afternoon project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-
+import './components/TopicBrowser/TopicBrowser'
+import TopicBrowser from './components/TopicBrowser/TopicBrowser';
 class App extends Component {
   render() {
-    
+    return <TopicBrowser/>
   }
 }
 
